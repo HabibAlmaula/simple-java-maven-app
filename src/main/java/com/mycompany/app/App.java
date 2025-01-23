91148a549d02ceedb66c5772cf42276134e60199
habibalmaula <habibalmaula@gmail.com>
2025-01-24 06:41:16 +0700

    Update Jenkinsfile and POM to enhance Docker configuration, rename deployment stage, and integrate Spring Boot in application

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,25 @@
 package com.mycompany.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  */
+@SpringBootApplication
+@RestController
 public class App {
-
     private static final String MESSAGE = "Hello World!";
 
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(MESSAGE);
-    }
-
+    @GetMapping("/")
     public String getMessage() {
         return MESSAGE;
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
+
